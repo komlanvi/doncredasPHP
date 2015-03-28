55857190e942d4808fd09ff33a5dce3b855ae5bd
doncredas <doncredas@gmail.com>
2015-03-28 03:22:03 +0100

    Implementing twig template in the project

--- Blog/app/app.php
+++ Blog/app/app.php
@@ -15,6 +15,9 @@ ExceptionHandler::register();
 
 // Register service providers.
 $app->register(new Silex\Provider\DoctrineServiceProvider());
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
 
 // Register services.
 $app['Dao.article'] = $app->share(function ($app) {
--- Blog/app/routes.php
+++ Blog/app/routes.php
@@ -2,9 +2,5 @@
 
 $app->get('/', function () use ($app) {
     $articles = $app['Dao.article']->findAll();
-
-    ob_start();
-    require __DIR__ .'/../views/view.php';
-    $view = ob_get_clean();
-    return $view;
+    return $app['twig']->render('index.html.twig', array('articles' => $articles));
 });
