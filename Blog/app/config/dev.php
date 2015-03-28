d686457ee8fa16d60ba7bc86a5358dee54ae2edc
doncredas <doncredas@gmail.com>
2015-03-28 03:01:54 +0100

    Adding the dev and prod environment

---
+++ Blog/app/config/dev.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: doncredas
+ * Date: 28/03/15
+ * Time: 2.00
+ */
+
+// include the prod configuration
+require __DIR__.'/prod.php';
+
+// enable the debug mode
+$app['debug'] = true;
---
+++ Blog/app/config/prod.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: doncredas
+ * Date: 28/03/15
+ * Time: 1.59
+ */
+
+// Doctrine (db)
+$app['db.options'] = array(
+    'driver'   => 'pdo_mysql',
+    'charset'  => 'utf8',
+    'host'     => 'localhost',
+    'port'     => '3306',
+    'dbname'   => 'myphp',
+    'user'     => 'doncredas',
+    'password' => 'secret',
+);
