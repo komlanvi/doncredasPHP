213102672be9371fde7d5256c29e5d86875bd9a8
doncredas <doncredas@gmail.com>
2015-03-28 01:06:56 +0100

    Adding the Article php class

---
+++ blog/src/domain/Article.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace doncredasPHP\blog\src\domain;
+
+
+class Article {
+
+    /**
+     * Article id
+     * @var string
+     */
+    private $id;
+
+    /**
+     * Article title
+     * @var string
+     */
+    private $titre;
+
+    /**
+     * Article content
+     * @var string
+     */
+    private $contenu;
+
+    /**
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * @param string $titre
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+
+    /**
+     * @param string $contenu
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+    }
+
+}
