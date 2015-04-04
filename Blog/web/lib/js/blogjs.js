34e45f884bfcddf83a5e04477c5386fe74b0432f
doncredas <doncredas@gmail.com>
2015-04-04 14:53:31 +0200

    The blog js file

---
+++ Blog/web/lib/js/blogjs.js
@@ -0,0 +1,10 @@
+/**
+ * Created by doncredas on 04/04/15.
+ */
+jQuery(function ($) {
+    $("#main").css({
+        fontSize: 17 + "px",
+        lineHeight: 1.6,
+        backgroundColor: "#FFF"
+    });
+});
