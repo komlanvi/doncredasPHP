5cb064c0e66181d716c3faf832c79c3f8ca666e2
doncredas <doncredas@gmail.com>
2015-04-04 15:00:36 +0200

    The comment data access object

--- Blog/src/Dao/CommentDAO.php
+++ Blog/src/Dao/CommentDAO.php
@@ -19,10 +19,44 @@ class CommentDAO extends DAO{
     private $articleDAO;
 
     /**
-     * @param \Blog\Dao\ArticleDAO $articleDAO
+     * @var \Blog\Dao\UserDAO
+     */
+    private $userDAO;
+
+    /**
+     * @param ArticleDAO $articleDAO
      */
     public function setArticleDAO(ArticleDAO $articleDAO) {
         $this->articleDAO = $articleDAO;
+    }
+
+    /**
+     * @param UserDAO $userDAO
+     */
+    public function setUserDAO (UserDAO $userDAO) {
+        $this->userDAO = $userDAO;
+    }
+
+    /**
+     * @param Comment $comment
+     */
+    public function save(Comment $comment) {
+        $commentData = array(
+            'article_id' => $comment->getArticle()->getId(),
+            'user_id' => $comment->getAuthor()->getId(),
+            'content' => $comment->getContent()
+        );
+
+        if ($comment->getId()) {
+            // The comment has already been saved : update it
+            $this->getDb()->update('comment', $commentData, array('id' => $comment->getId()));
+        } else {
+            // The comment has never been saved : insert it
+            $this->getDb()->insert('comment', $commentData);
+            // Get the id of the newly created comment and set it on the entity.
+            $id = $this->getDb()->lastInsertId();
+            $comment->setId($id);
+        }
     }
 
     /**
@@ -34,7 +68,7 @@ class CommentDAO extends DAO{
      * @return array comments
      */
     public function findAllByArticle($articleId) {
-        $sql = "SELECT * FROM comment WHERE id=?";
+        $sql = "SELECT * FROM comment WHERE article_id = ?";
         $results = $this->getDB()->fetchAll($sql, array($articleId));
 
         $comments = array();
@@ -47,19 +81,26 @@ class CommentDAO extends DAO{
 
     /**
      * @param array $row
-     * @return array $comments
+     * @return array $comment
      */
     protected function buildDomainObject(array $row){
         $comment = new Comment();
         $comment->setId($row["id"]);
-        $comment->setAuthor($row["author"]);
         $comment->setContent($row["content"]);
 
-        if(array_key_exists("article_id", $row)){
+
+        if(array_key_exists("article_id", $row)) {
             $articleId = $row["article_id"];
             $article = $this->articleDAO->findArticle($articleId);
             $comment->setArticle($article);
         }
+
+        if(array_key_exists("user_id", $row)) {
+            $userId = $row["user_id"];
+            $author = $this->userDAO->findUserById($userId);
+            $comment->setAuthor($author);
+        }
+
         return $comment;
     }
 
