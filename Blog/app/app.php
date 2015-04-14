ad79bd60cad6867164f21615a4fc3d3289c5d21c
doncredas <doncredas@gmail.com>
2015-04-15 01:26:50 +0200

    Added the error handler and the json parse function

--- Blog/app/app.php
+++ Blog/app/app.php
@@ -8,6 +8,7 @@
 
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
+use Symfony\Component\HttpFoundation\Request;
 
 // Register global error and exception handlers
 ErrorHandler::register();
@@ -18,11 +19,6 @@ $app->register(new Silex\Provider\DoctrineServiceProvider());
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
-
-$app['twig'] = $app->share($app->extend('twig', function(Twig_Environment $twig, $app) {
-    $twig->addExtension(new Twig_Extensions_Extension_Text());
-    return $twig;
-}));
 
 $app->register(new Silex\Provider\ValidatorServiceProvider());
 
@@ -52,7 +48,26 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     ),
 ));
 
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__.'/../var/logs/blog.log',
+    'monolog.name' => 'Blog',
+    'monolog.level' => $app['monolog.level']
+));
+$app->register(new Silex\Provider\ServiceControllerServiceProvider());
+if (isset($app['debug']) && $app['debug']) {
+    $app->register(new Silex\Provider\WebProfilerServiceProvider(), array(
+        'profiler.cache_dir' => __DIR__.'/../var/cache/profiler'
+    ));
+}
+
+
 // Register services.
+
+$app['twig'] = $app->share($app->extend('twig', function(Twig_Environment $twig, $app) {
+    $twig->addExtension(new Twig_Extensions_Extension_Text());
+    return $twig;
+}));
+
 $app['Dao.article'] = $app->share(function ($app) {
     return new Blog\Dao\ArticleDAO($app['db']);
 });
@@ -67,3 +82,27 @@ $app['Dao.comment'] = $app->share(function ($app) {
     $commentDAO->setUserDAO($app['Dao.user']);
     return $commentDAO;
 });
+
+// Register error handler
+$app->error(function (Exception $e, $code) use ($app) {
+    switch ($code) {
+        case 403:
+            $message = 'Access denied.';
+            break;
+        case 404:
+            $message = 'The requested resource could not be found.';
+            break;
+        default:
+            $message = "Something went wrong.";
+    }
+    return $app['twig']->render('error.html.twig', array('message' => $message, 'code' => $code));
+});
+
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
+
