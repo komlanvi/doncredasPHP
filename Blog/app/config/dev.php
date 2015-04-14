30888c357be523687764596ced60384e87c4e977
doncredas <doncredas@gmail.com>
2015-04-15 01:29:10 +0200

    Set the log level for the dev environement

--- Blog/app/config/dev.php
+++ Blog/app/config/dev.php
@@ -6,8 +6,19 @@
  * Time: 2.00
  */
 
-// include the prod configuration
-require __DIR__ . '/prod.php';
+// Doctrine (db)
+$app['db.options'] = array(
+    'driver'   => 'pdo_mysql',
+    'charset'  => 'utf8',
+    'host'     => '127.0.0.1',  // Mandatory for PHPUnit testing
+    'port'     => '3306',
+    'dbname'   => 'myphp',
+    'user'     => 'doncredas',
+    'password' => 'secret'
+);
 
 // enable the debug mode
 $app['debug'] = true;
+
+//Log level
+$app['monolog.level'] = 'INFO';
