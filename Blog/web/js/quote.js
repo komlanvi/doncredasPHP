7e919a07a47fe759180d186b1cdbddf3118d21cb
doncredas <doncredas@gmail.com>
2015-04-21 19:44:14 +0200

    My quotes js config file

--- Blog/web/js/quote.js
+++ Blog/web/js/quote.js
@@ -2,15 +2,8 @@
  * Created by doncredas on 04/04/15.
  */
 jQuery(function ($) {
-    $("#main").css({
-        fontSize: 17 + "px",
-        lineHeight: 1.6,
-        backgroundColor: "#FFF"
-    });
-
     //Set the carousel options
     $('#quote-carousel').carousel({
-        pause: true,
-        interval: 4000
+        interval: 3000
     });
 });
